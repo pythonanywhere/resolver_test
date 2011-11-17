f6f1c4fb2060ac1d778902fcd9659d1966f844d7
Hansel Dunlop <hansel.dunlop@resolversystems.com>
2011-11-17 15:30:19 +0000

    Moved test utils and generalised test utils into resolver test utils. By Giles & Hansel.

---
+++ README.md
@@ -0,0 +1,1 @@
+Resolver Test utilities for Django. 
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import find_packages, setup  
+
+setup(
+    name='resolver_test',
+    version='1.0',         
+    packages=find_packages()
+)
