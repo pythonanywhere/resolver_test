4bd80b70bdce5f7f13c40ada1f544e35ad1df884
Harry <hjwp2@cantab.net>
2015-12-28 09:32:23 +0000

    T4604 strip out some old-django and other unused stuff. by: Conrad, Harry

--- resolver_test/__init__.py
+++ resolver_test/__init__.py
@@ -2,24 +2,17 @@
 # All Rights Reserved
 #
 
-try:
-    import unittest2 as unittest
-except ImportError:
-    import unittest
-
+import unittest
 from datetime import timedelta
 from mock import call
 
 
 class ResolverTestMixins(object):
+
     def assertCalledOnce(self, mock, *args, **kwargs):
         if mock.call_args_list == []:
             self.fail('Not called')
         self.assertEquals(mock.call_args_list, [call(*args, **kwargs)])
-
-
-    def assert_decorated_with(self, fn, decorator):
-        self.assertIn(decorator.__name__, fn.decorated_by)
 
 
     def assert_decorated_with(self, fn, decorator):
@@ -42,9 +35,11 @@ class ResolverTestCase(unittest.TestCase, ResolverTestMixins):
 def die(exception=None):
     if exception is None:
         exception = AssertionError('die called')
+
     def inner_die(*_, **__):
         raise exception
     return inner_die
+
 
 def is_iterable(seq):
     return hasattr(seq, "__iter__")
